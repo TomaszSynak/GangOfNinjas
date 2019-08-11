7b82b775cc276f0b2c108e2cd5910fd4533cfa3e
Tomasz Synak <tomasz.synak@outlook.com>
2019-08-11 19:23:00 +0200

    Basic Traefik configuration

--- Projects/HiddenNinja/Controllers/HealthController.cs
+++ Projects/HiddenNinja/Controllers/HealthController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,7 +13,11 @@ namespace HiddenNinja.Controllers
         public async Task Get()
         {
             Response.StatusCode = 200;
-            await Response.WriteAsync($"Healthy <br><br> --- <br> Best <br> <i><small>{nameof(HiddenNinja)}</small></i>");
+            await Response.WriteAsync(
+            "Healthy \n\r" +
+                $"Headers: \n\r {string.Join("\n", Request.Headers.ToList().Select(h => $"{h.Key} - {h.Value}"))} \n\r" +
+                " --- Best \n\r" +
+                $"{nameof(HiddenNinja)}");
         }
     }
 }
