c0845976ecbe1525fc7704099de24d7febb91bc7
Tomasz Synak <tomasz.synak@outlook.com>
2019-08-11 18:40:28 +0200

    Add new service (HiddenSamurai)

---
+++ Projects/HiddenSamurai/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HiddenSamurai.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task Get()
+        {
+            Response.StatusCode = 200;
+            await Response.WriteAsync($"Healthy <br><br> --- <br> Best <br> <i><small>{nameof(HiddenSamurai)}</small></i>");
+        }
+    }
+}
